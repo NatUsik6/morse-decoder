9c9c5cd9f67ffffc48ef7d9fcc0e95903afcdae5
NatUsik6 <tashausik6@gmail.com>
2023-02-18 22:42:43 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let encodedWords = expr.split('**********');
+    let decodedWords = [];
+    for (let encodedWord of encodedWords) {
+        let decodedWord = '';
+        for (let i = 0; i < encodedWord.length; i += 10) {
+            let encodedLetter = encodedWord.substr(i, 10);
+            let morseLetter = '';
+            for (let j = encodedLetter.indexOf('1'); j < encodedLetter.length; j += 2) {
+                morseLetter += encodedLetter[j + 1] == '0' ? '.' : '-';
+            }
+
+            decodedWord += MORSE_TABLE[morseLetter];
+        }
+
+        decodedWords.push(decodedWord);
+    }
+
+    return decodedWords.join(' ');
 }
 
 module.exports = {
